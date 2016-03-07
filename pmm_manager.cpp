c5e83ba9e73ae920ce55a760b52d151d50fb4d1e
Silver-Shen <shenzy13@qq.com>
2016-03-07 14:01:50 +0800

    xxx

--- pmm_manager.cpp
+++ pmm_manager.cpp
@@ -18,28 +18,32 @@ struct Buddy_system
     int total_size, epsilon;
 
     void init_buddy(buddy_node* node, int l, int r){
-    node->start = l;
-    node->end = r;
-    node->largest_free_size = r-l+1;
-    node->total_alloc = false;
-    if (r-l+1>epsilon){
-        node->left = new buddy_node;
-        node->right = new buddy_node;
-        init_buddy(node->left, l, (l+r)/2);
-        init_buddy(node->right, (l+r)/2+1, r);
-    }else{
-        node->left = NULL;
-        node->right = NULL;
-    }
+        node->start = l;
+        node->end = r;
+        node->largest_free_size = r-l+1;
+        node->total_alloc = false;
+        if (r-l+1>epsilon){
+            node->left = new buddy_node;
+            node->right = new buddy_node;
+            init_buddy(node->left, l, (l+r)/2);
+            init_buddy(node->right, (l+r)/2+1, r);
+        }else{
+            node->left = NULL;
+            node->right = NULL;
+        }
     }
 
     void traverse(buddy_node* node){
-    cout << node->start << ' ' << node->end << ' ' << node->largest_free_size << endl;
-    if (node->left) traverse(node->left);
-    if (node->right) traverse(node->right);
+        cout << node->start << ' ' << node->end << ' ' << node->largest_free_size << endl;
+        if (node->left) traverse(node->left);
+        if (node->right) traverse(node->right);
     }
 
     int malloc(int size){
+        __
+    }
+    
+    int __malloc(buddy_node* node, int size){
 
     }
 
@@ -56,5 +60,6 @@ int main(int argc, char const *argv[])
     buddy_system.root = new buddy_node;
     buddy_system.init_buddy(buddy_system.root, 0, buddy_system.total_size-1);
     buddy_system.traverse(buddy_system.root);
+    int address = buddy_system.malloc(32);
     return 0;
 }
