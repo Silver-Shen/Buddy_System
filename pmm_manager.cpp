2063a05662e85e13dcaa5653c7d4899b2fce7302
qq775193759 <775193759@qq.com>
2016-03-07 14:42:36 +0800

    add free

--- pmm_manager.cpp
+++ pmm_manager.cpp
@@ -21,6 +21,7 @@ struct Buddy_system
         node->start = l;
         node->end = r;
         node->largest_free_size = r-l+1;
+        //node->total_alloc = false;
         node->total_alloc = false;
         if (r-l+1>epsilon){
             node->left = new buddy_node;
@@ -34,15 +35,14 @@ struct Buddy_system
     }
 
     void traverse(buddy_node* node){
-        cout << node->start << ' ' << node->end << ' ' << node->largest_free_size << endl;
+        cout << node->start << ' ' << node->end << ' ' << node->largest_free_size << ' ' << node->total_alloc << endl;
         if (node->left) traverse(node->left);
         if (node->right) traverse(node->right);
     }
 
     int malloc(int size){
-        __
     }
-    
+
     int __malloc(buddy_node* node, int size){
 
     }
@@ -51,12 +51,33 @@ struct Buddy_system
         __free(addr, root);
     }
 
-    void __free(int addr, buddy_node* node){
-        if((node->start == addr)){
-
+    int __free(int addr, buddy_node* node){
+        if((node->start == addr) && (node->total_alloc == 1)){
+            node->total_alloc = 0;
+            node->largest_free_size = (node->end - node->start +1);
+            return 1;
         }else{
+            if(addr > node->left->end){
+                if(__free(addr, node->right)){
+                    if(node->left->largest_free_size == (node->left->end - node->left->start +1)){
+                        node->largest_free_size = (node->end - node->start +1);
+                        return 1;
+                    }else{
+                        node->largest_free_size = max(node->left->largest_free_size, node->right->largest_free_size);
+                    }
+                }
+            }else{
+                if(__free(addr, node->left)){
+                    if(node->right->largest_free_size == (node->right->end - node->right->start +1)){
+                        node->largest_free_size = (node->end - node->start +1);
+                        return 1;
+                    }else{
+                        node->largest_free_size = max(node->left->largest_free_size, node->right->largest_free_size);
+                    }
+                }
+            }
         }
-
+        return 0;
     }
 }buddy_system;
 
@@ -67,6 +88,7 @@ int main(int argc, char const *argv[])
     //initialize the root node
     buddy_system.root = new buddy_node;
     buddy_system.init_buddy(buddy_system.root, 0, buddy_system.total_size-1);
+    buddy_system.free(96);
     buddy_system.traverse(buddy_system.root);
     int address = buddy_system.malloc(32);
     return 0;
