79df64aa5487050a1bba907e1c5d89dee1d5de90
qq775193759 <775193759@qq.com>
2016-03-07 13:56:05 +0800

    add structure

--- pmm_manager.cpp
+++ pmm_manager.cpp
@@ -1,4 +1,4 @@
-#include <iostream> 
+#include <iostream>
 #include <cstdlib>
 using namespace std;
 
@@ -11,10 +11,12 @@ struct buddy_node
     int largest_free_size;
 };
 
-buddy_node* root;
-int total_size, epsilon;
+struct Buddy_system
+{
+    buddy_node* root;
+    int total_size, epsilon;
 
-void init_buddy(buddy_node* node, int l, int r){
+    void init_buddy(buddy_node* node, int l, int r){
     node->start = l;
     node->end = r;
     node->largest_free_size = r-l+1;
@@ -27,29 +29,30 @@ void init_buddy(buddy_node* node, int l, int r){
         node->left = NULL;
         node->right = NULL;
     }
-}
- 
-void traverse(buddy_node* node){
+    }
+
+    void traverse(buddy_node* node){
     cout << node->start << ' ' << node->end << ' ' << node->largest_free_size << endl;
     if (node->left) traverse(node->left);
     if (node->right) traverse(node->right);
-}
+    }
 
-int malloc(int size){
+    int malloc(int size){
 
-}
+    }
 
-void free(){
+    void free(int addr){
 
-}
+    }
+}buddy_system;
 
 int main(int argc, char const *argv[])
 {
     cout << "Please enter the total size and threshold:" << endl;
-    cin >> total_size >> epsilon;
+    cin >> buddy_system.total_size >> buddy_system.epsilon;
     //initialize the root node
-    root = new buddy_node;    
-    init_buddy(root, 0, total_size-1);
-    traverse(root);
+    buddy_system.root = new buddy_node;
+    buddy_system.init_buddy(buddy_system.root, 0, buddy_system.total_size-1);
+    buddy_system.traverse(buddy_system.root);
     return 0;
 }
